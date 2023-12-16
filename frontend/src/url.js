461c5d79ad0b010a42c2a01afd323bd3eaa2b1d5
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2023-12-16 19:41:49 +0530

    last

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://rj-rahul-jauhari-portfolio-api-rahul-jauharis-projects.vercel.app'
+export const BASE_URL = 'https://rj-rahul-jauhari-portfolio-chj1pd3hm-rahul-jauharis-projects.vercel.app'
 
 // http://localhost:5000
