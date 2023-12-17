ada48d1f0f3306c02fbe3f992878539e2bb55995
Rahul Jauhari <rj.rahul.jauhari@gmail.com>
2023-12-17 14:47:51 +0530

    yjtuk

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000'
+export const BASE_URL = 'https://rj-portfolio-api.onrender.com'
 
 // http://localhost:5000
 // https://rj-portfolio-api.onrender.com
